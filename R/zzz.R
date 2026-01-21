9f13dace578308bcd36ca8eddefd7df3caed7a7d
cmndrsn <42416372+cmndrsn@users.noreply.github.com>
2026-01-20 20:04:13 -0500

    chore: update messages

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("This package is under active development and some functionality remains in an experimental stage.\n
+    Please see https://github.com/cmndrsn/ggcommonality/issues for details")
+}
